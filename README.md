e3cea5dc0d5f1c3ffa669c2b22402d872cb15f70
pjhpey <pjhpey@gmail.com>
2025-04-09 18:14:05 +0900

    gh-pages deploy 추가

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pidosoft.github.io
+
+gh-pages에 업데이트 방법. 
+
+npm run deploy를 git-bash에서 실행할 것!
---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const config = {
+    output: 'export', // Enables static HTML export
+};
+
+export default config;
